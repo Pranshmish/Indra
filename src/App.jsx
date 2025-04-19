d80120c2a5641bb7651538bc44a52594167e346d
ujjuengineer <uk3945351@gmail.com>
2025-04-19 11:52:15 +0530

    added temprature card

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import Header from './component/Header'
-// import './App.css'
-// import Navbar from './Components/Navbar'
+import WetherCard from './component/wetherCard'
+
+
 
 export default function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header/>
+      <div className='m-8 border border-width: 1px'>
+        <Header />
+        <hr className='mx-3' />
+        <WetherCard />
+      </div>
+      
     </>
   )
 }
--- src/component/wetherCard.jsx
+++ src/component/wetherCard.jsx
@@ -3,10 +3,20 @@ import '../index.css';
 export default function Card () {
     return (
         <>
-            <div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div className="flex flex-row mx-2 bg-amber-50">
+                <div className='flex flex-col items-center border rounded-lg m-2 p-1'>
+                    <div>Day</div>
+                    <div><img className='w-5' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxBuT24LzHly7RNc3-464PLMntDFQlagfeDA&s" alt="" /></div>
+                    <div>Temprature</div>
+                </div>
+
+
+                {/* copy of previous card */}
+                <div className='flex flex-col items-center border rounded-lg m-2 p-1'>
+                    <div>Day</div>
+                    <div><img className='w-5' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxBuT24LzHly7RNc3-464PLMntDFQlagfeDA&s" alt="" /></div>
+                    <div>Temprature</div>
+                </div>
             </div>
         </>
     )
