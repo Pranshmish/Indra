c061534801957e0e23b74cfe81abb202cf46449c
Pranshmish <pragya636@gmail.com>
2025-04-21 23:19:58 +0530

    asdwqe

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 
 import Home from "./Pages/Home";
-import RainScene from "./components/3D/Render/RainRenderer";
-import SnowRender from "./components/3D/Render/SnowRenderer";
+
 const App = () => {
   
 
   return (
   <>
   <Home />
-  {/* <SnowRender/> */}
-  {/* <RainScene/> */}
   </>
   );
 };
